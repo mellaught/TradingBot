17c5642fa0883f9ec9772f4f894ae86e209328ac
uabeautifull <torres-dan@yandex.ru>
2019-11-11 16:43:59 +0300

    add welcome message

--- src/app/app.go
+++ src/app/app.go
@@ -2,27 +2,26 @@ package app
 
 import (
 	"TradingBot/src/models"
-	"tradingBot/src/bot"
+	"tradingBot/src/app/bot"
 )
 
 // Struct for Tranding Bot.
 type App struct {
-	Bot     *bot.Bot
-	Binance *Binance.
+	Bot *bot.Bot
 }
 
 // InitService is initializes the app.
-func NewApp(conf *models.Config, members models.Members) *App {
+func NewApp(conf *models.Config, members *models.Members) *App {
 
 	a := App{
-		Bot: *bot.Bot{},
+		Bot: &bot.Bot{},
 	}
 
 	// Start Bot.
-	a.Bot = bot.InitBot()
+	a.Bot = bot.InitBot(conf.Bot, members)
 	go a.Bot.Run()
 	// Start Binance Exchange.
-	go a.Exchanges.Binance.Run()
+	//go a.Exchanges.Binance.Run()
 
 	return &a
 }
--- src/app/bot/message-texts.go
+++ src/app/bot/message-texts.go
@@ -1,7 +1,8 @@
 package bot
 
 var (
-	startMessage = "Hello!\n\nWelcome to **Tranding Bot** by D. Gorokhov."
+	startMessage = "Hello!\n\nPlease enter password..."
+	welcomeMessage = "Welcome to **Tranding Bot** by D. Gorokhov."
 	menuMessage = "Balance:\n\n1.USDT: **%.2f**\n2.Bitcoin: **%.2f**"
 	notifyMessage = "**ON** or **OFF** Notifications from Trading BOT."
 	OFFMessage = "Attention❗❗❗\n\nIF you stop the bot, will restart all APP."
